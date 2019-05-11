2ce1a99916cc86a859e746dd855553a382d56535
Noah Lidell <noahlidell@gmail.com>
2019-05-11 18:07:00 -0500

    listing scripts

--- ls_cards.py
+++ ls_cards.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 sys.path.append("anki")
 import anki
-import anki.sched
+import anki.sched # not sure why i have to do this step...
 
 Collection  = anki.storage.Collection
 
@@ -20,7 +20,33 @@ col = Collection(cpath, log=True) # Entry point to the API
 
 # Use the available methods to list the notes
 #for cid in col.findNotes("tag:English"): 
-for cid in col.findNotes("tag:*"): 
-    note = col.getNote(cid)
-    front =  note.fields[0] # "Front" is the first field of these cards
+for cid in col.findCards("tag:*"): 
+    card = col.getCard(cid)
+    note = col.getNote(card.nid)
+    front = note.fields[0]
+    model = col.models.get(note.mid)
+
+    print("//////////////////////////////////////////")
+    print("//////////////////////////////////////////")
+    print(card)
+    print("//////////////////////////////////////////")
+    print(note)
     print(front)
+    print("//////////////////////////////////////////")
+    print(model)
+    print("//////////////////////////////////////////")
+    try:
+        template = model['tmpls'][card.ord]
+    except IndexError:
+
+        print("............INDEX ERR ON TEMPLATE.........")
+        print(model['tmpls'])
+        print(".")
+        print(card.ord)
+        print(".")
+        print(card)
+        print(".")
+        print(front)
+        print("..........................................")
+        template = model['tmpls'][0]
+    print(template)
