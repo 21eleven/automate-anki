35d551f5959ce0598faab8194cd09d252769d030
Noah Lidell <noahlidell@gmail.com>
2019-06-16 17:59:09 -0500

    working pull of color descripton from color hexa

--- colorhexa.py
+++ colorhexa.py
@@ -1,7 +1,18 @@
 import sys
+import requests
+from bs4 import BeautifulSoup
+
+def get_color_desc(color):
+    colorhexa_url = "https://www.colorhexa.com/color.php"
+    res = requests.post(colorhexa_url, data={'color-picker': '', 'c': color, 'h': 'h'})
+    soup = BeautifulSoup(res.text, 'html.parser')
+    desc = soup.find_all("div", {"class" :"color-description"})
+    print(desc)
+
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         color = sys.argv[1]
         print(color)
+        get_color_desc(color)
     else:
         print(sys.argv)
